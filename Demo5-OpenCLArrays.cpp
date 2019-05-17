e2368e564c6e82e4c2eb048c83fca30957a3c069
Eric Morgan <accounts@ericcolvinmorgan.com>
2019-05-17 10:55:00 +0200

    Added double reduction

--- Demo5-OpenCLArrays.cpp
+++ Demo5-OpenCLArrays.cpp
@@ -19,7 +19,7 @@
 #define ID_NVIDIA	0x10de
 
 #ifndef SELECTED_PLATFORM
-#define	SELECTED_PLATFORM		0
+#define	SELECTED_PLATFORM		1
 #endif
 
 #ifndef SELECTED_DEVICE
@@ -27,7 +27,7 @@
 #endif
 
 #ifndef NUM_ELEMENTS
-#define NUM_ELEMENTS		1024
+#define NUM_ELEMENTS		8388608
 #endif
 
 #ifndef LOCAL_SIZE
@@ -45,6 +45,7 @@ void				Wait( cl_command_queue );
 void				ArrayMulti();
 void				ArrayMultiAdd();
 void				ArrayMultiReduce();
+void				ArrayMultiReduceDouble();
 int				LookAtTheBits( float );
 void PrintPlatformDeviceInfo(cl_platform_id*, cl_uint);
 
@@ -73,6 +74,7 @@ int main( int argc, char *argv[ ] )
 	ArrayMulti();
 	ArrayMultiAdd();
 	ArrayMultiReduce();
+	ArrayMultiReduceDouble();
 
 	return 0;
 }
@@ -673,11 +675,220 @@ void ArrayMultiReduce()
 
 	if (fabs(sum - expected) > TOL)
 	{
-		//fprintf(stderr, "%13.6f wrongly produced instead of %13.6f (%13.8f)\n",
-		//	sum, expected, fabs(sum - expected));
+		fprintf(stderr, "%13.6f wrongly produced instead of %13.6f (%13.8f)\n",
+			sum, expected, fabs(sum - expected));
 	}
 
 	printf("ArrayMultiReduce,%d,%d,%d,%lf\n", NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, (double)NUM_ELEMENTS / (time1 - time0) / 1000000000.);
+
+
+#ifdef WIN32
+	Sleep(2000);
+#endif
+
+
+	// 13. clean everything up:
+
+	clReleaseKernel(kernel);
+	clReleaseProgram(program);
+	clReleaseCommandQueue(cmdQueue);
+	clReleaseMemObject(dA);
+	clReleaseMemObject(dB);
+	clReleaseMemObject(dC);
+
+	delete[] hA;
+	delete[] hB;
+	delete[] hC;
+}
+
+void ArrayMultiReduceDouble()
+{
+	// see if we can even open the opencl kernel program
+	// (no point going on if we can't):
+
+	FILE* fp;
+#ifdef WIN32
+	errno_t err = fopen_s(&fp, CL_FILE_NAME, "r");
+	if (err != 0)
+#else
+	fp = fopen(CL_FILE_NAME, "r");
+	if (fp == NULL)
+#endif
+	{
+		fprintf(stderr, "Cannot open OpenCL source file '%s'\n", CL_FILE_NAME);
+	}
+
+	cl_int status;
+	// returned status from opencl calls
+	// test against CL_SUCCESS
+	// get the platform id:
+	cl_platform_id platform[MAX_PLATFORMS];
+	cl_uint platforms;
+	status = clGetPlatformIDs(MAX_PLATFORMS, platform, &platforms);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clGetPlatformIDs failed (2) - Status Code:%d\n", (int)status);
+
+	// get the device id:
+	cl_device_id device[MAX_DEVICES];
+	cl_uint devices;
+	status = clGetDeviceIDs(platform[SELECTED_PLATFORM], CL_DEVICE_TYPE_GPU, MAX_DEVICES, device, &devices);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clGetDeviceIDs failed (2)\n");
+
+	// 2. allocate the host memory buffers:
+	size_t numWorkGroups = NUM_ELEMENTS / LOCAL_SIZE;
+	double* hA = new double[NUM_ELEMENTS];
+	double* hB = new double[NUM_ELEMENTS];
+	double* hC = new double[numWorkGroups];
+	size_t abSize = NUM_ELEMENTS * sizeof(double);
+	size_t cSize = numWorkGroups * sizeof(double);
+
+	// fill the host memory buffers:
+	for (int i = 0; i < NUM_ELEMENTS; i++)
+	{
+		hA[i] = hB[i] = sqrt((double)i);
+	}
+
+	// 3. create an opencl context:
+
+	cl_context context = clCreateContext(NULL, 1, &device[SELECTED_DEVICE], NULL, NULL, &status);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clCreateContext failed\n");
+
+	// 4. create an opencl command queue:
+
+	cl_command_queue cmdQueue = clCreateCommandQueue(context, device[SELECTED_DEVICE], 0, &status);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clCreateCommandQueue failed\n");
+
+	// 5. allocate the device memory buffers:
+
+	cl_mem dA = clCreateBuffer(context, CL_MEM_READ_ONLY, abSize, NULL, &status);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clCreateBuffer failed (1)\n");
+
+	cl_mem dB = clCreateBuffer(context, CL_MEM_READ_ONLY, abSize, NULL, &status);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clCreateBuffer failed (2)\n");
+
+	cl_mem dC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, cSize, NULL, &status);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clCreateBuffer failed (3)\n");
+
+	// 6. enqueue the 2 commands to write the data from the host buffers to the device buffers:
+
+	status = clEnqueueWriteBuffer(cmdQueue, dA, CL_FALSE, 0, abSize, hA, 0, NULL, NULL);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clEnqueueWriteBuffer failed (1)\n");
+
+	status = clEnqueueWriteBuffer(cmdQueue, dB, CL_FALSE, 0, abSize, hB, 0, NULL, NULL);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clEnqueueWriteBuffer failed (2)\n");
+
+	Wait(cmdQueue);
+
+	// 7. read the kernel code from a file:
+
+	fseek(fp, 0, SEEK_END);
+	size_t fileSize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	char* clProgramText = new char[fileSize + 1];		// leave room for '\0'
+	size_t n = fread(clProgramText, 1, fileSize, fp);
+	clProgramText[fileSize] = '\0';
+	fclose(fp);
+	if (n != fileSize)
+		fprintf(stderr, "Expected to read %d bytes read from '%s' -- actually read %d.\n", (int)fileSize, CL_FILE_NAME, (int)n);
+
+	// create the text for the kernel program:
+
+	char* strings[1];
+	strings[0] = clProgramText;
+	cl_program program = clCreateProgramWithSource(context, 1, (const char**)strings, NULL, &status);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clCreateProgramWithSource failed\n");
+	delete[] clProgramText;
+
+	// 8. compile and link the kernel code:
+
+	char optionsString[] = "";
+	char* options = optionsString;
+	status = clBuildProgram(program, 1, &device[SELECTED_DEVICE], options, NULL, NULL);
+	if (status != CL_SUCCESS)
+	{
+		size_t size;
+		clGetProgramBuildInfo(program, device[SELECTED_DEVICE], CL_PROGRAM_BUILD_LOG, 0, NULL, &size);
+		cl_char* log = new cl_char[size];
+		clGetProgramBuildInfo(program, device[SELECTED_DEVICE], CL_PROGRAM_BUILD_LOG, size, log, NULL);
+		fprintf(stderr, "clBuildProgram failed:\n%s\n", log);
+		delete[] log;
+	}
+
+	// 9. create the kernel object:
+	cl_kernel kernel = clCreateKernel(program, "ArrayMultReduceDouble", &status);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clCreateKernel failed\n");
+
+	// 10. setup the arguments to the kernel object:
+
+	status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &dA);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clSetKernelArg failed (1)\n");
+
+	status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &dB);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clSetKernelArg failed (2)\n");
+
+	status = clSetKernelArg(kernel, 2, LOCAL_SIZE * sizeof(double), NULL);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clSetKernelArg failed (3)\n");
+
+	status = clSetKernelArg(kernel, 3, sizeof(cl_mem), &dC);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clSetKernelArg failed (4)\n");
+
+
+	// 11. enqueue the kernel object for execution:
+
+	size_t globalWorkSize[3] = { NUM_ELEMENTS, 1, 1 };
+	size_t localWorkSize[3] = { LOCAL_SIZE,   1, 1 };
+
+	Wait(cmdQueue);
+	double time0 = omp_get_wtime();
+
+	status = clEnqueueNDRangeKernel(cmdQueue, kernel, 1, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clEnqueueNDRangeKernel failed: %d\n", status);
+
+	Wait(cmdQueue);
+	double time1 = omp_get_wtime();
+
+	// 12. read the results buffer back from the device to the host:
+
+	status = clEnqueueReadBuffer(cmdQueue, dC, CL_TRUE, 0, numWorkGroups * sizeof(double), hC, 0, NULL, NULL);
+	if (status != CL_SUCCESS)
+		fprintf(stderr, "clEnqueueReadBuffer failed\n");
+	Wait(cmdQueue);
+
+	double sum = 0.;
+	for (int i = 0; i < numWorkGroups; i++)
+	{
+		sum += hC[i];
+	}
+
+	// did it work?
+	double expected = 0.;
+	for (int i = 0; i < NUM_ELEMENTS; i++)
+	{
+		expected += (hA[i] * hB[i]);
+	}
+
+	if (fabs(sum - expected) > TOL)
+	{
+		fprintf(stderr, "%13.6f wrongly produced instead of %13.6f (%13.8f)\n",
+			sum, expected, fabs(sum - expected));
+	}
+
+	printf("ArrayMultiReduceDouble,%d,%d,%d,%lf\n", NUM_ELEMENTS, LOCAL_SIZE, NUM_WORK_GROUPS, (double)NUM_ELEMENTS / (time1 - time0) / 1000000000.);
 
 
 #ifdef WIN32
